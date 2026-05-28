a415300ef76e2c153699a725f62c25a9832baecd
Flipps12 <paginasypruebas12@gmail.com>
2026-05-28 16:54:43 -0300

    C:/Program Files/Git/calificaiones /alumno/id/calificacion

--- gestion_academica/urls.py
+++ gestion_academica/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ListadoAlumnosView, ImportarAlumnosView, EditarAlumnoView, ProfesorListView, ListaCargosView
+from .views import DetalleCalificacionesAlumnoView, ListadoAlumnosView, ListaCalificacionesView, ImportarAlumnosView, EditarAlumnoView, ProfesorListView, ListaCargosView
 
 urlpatterns = [
+    path('alumno/<int:pk>/calificaciones/', DetalleCalificacionesAlumnoView.as_view(), name='detalle_calificaciones_alumno'),
     path('alumnos/', ListadoAlumnosView.as_view(), name='listado_alumnos'),
     path('alumnos/importar_alumnos/', ImportarAlumnosView.as_view(), name='importar_alumnos'),
     path('alumnos/editar/<int:pk>/', EditarAlumnoView.as_view(), name='editar_alumno'),
     path('profesores/', ProfesorListView.as_view(), name='listado_profesor'),
     path('cargos/', ListaCargosView.as_view(), name='lista_cargos'),
+    path('calificaciones/', ListaCalificacionesView.as_view(), name='lista_calificaciones'),
 ]
